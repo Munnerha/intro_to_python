40583d286cdf26a95f21ae2080b1ed70654e55e6
Muneer <85256095+Munnerha@users.noreply.github.com>
2026-09-20 13:41:52 +0300

    Yes

---
+++ intro_to_python.py
@@ -0,0 +1,25 @@
+#hello how are you
+#bla bla bla
+
+
+"""
+this is a
+multiline comment
+"""
+
+'''
+sadsadasdasd
+'''
+#print(2)
+
+color = input('Enter "green", "yellow", "red": ').lower()
+print(f'The user entered {color}')
+
+if color == "green":
+  print('Go!')
+elif color == "yellow":
+  print('Slow Down!')
+elif color == "red":
+  print('Stop!')
+else:
+  print('Bogus!') 
